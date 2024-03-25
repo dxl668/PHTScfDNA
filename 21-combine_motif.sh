d4c94e77455652a8b6b30bb5d990bb232f75b2ff
Darren Liu <64102138+dxl668@users.noreply.github.com>
2024-03-25 14:51:52 -0400

    Update 21-combine_motif.sh

--- 21-combine_motif.sh
+++ 21-combine_motif.sh
@@ -1,17 +1,18 @@
-#load R
+#!/bin/bash
+
+# Define project directory
+PROJECT_DIR=/home/liud3/beegfs/cfDNA/protocol
+cd "$PROJECT_DIR"
+
+# Define input/output directories 
+motifdir="${PROJECT_DIR}/20-motif_gc"
+outdir="${PROJECT_DIR}/21-combine_motif"
+mkdir -p "$outdir"  #Create directories if needed
+
 module load R
 
-#Define my working directory 
-CWD=/home/liud3/beegfs/cfDNA/data/cfdna_pipeline
-cd $CWD
-
-
-#Define directory for sample end motifs 
-motifdir=/home/liud3/beegfs/cfDNA/data/cfdna_pipeline/20-motif_gc_correct
-
-#Define out directory
-outdir=/home/liud3/beegfs/cfDNA/data/cfdna_pipeline/21-combine_motif
-
-#Run R script 
-Rscript 21-combine_motif.R --motifdir $motifdir --outdir $outdir
-echo Done combine dataframes for motif summaries!
+# Initialize R script
+Rscript 21-combine_motif.R \
+--motifdir $motifdir \
+--outdir $outdir
+echo Done combining end motif data!
