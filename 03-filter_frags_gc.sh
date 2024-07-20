e43d16805d0f60dd9f236bb48de57551b6822a14
Darren Liu <64102138+dxl668@users.noreply.github.com>
2024-07-20 15:24:24 -0400

    Update 03-filter_frags_gc.sh

--- 03-filter_frags_gc.sh
+++ 03-filter_frags_gc.sh
@@ -17,7 +17,7 @@
 # #SBATCH --mail-user=your.email@example.com
 
 # Define project directory
-PROJECT_DIR=/user
+PROJECT_DIR=/home/liud3/beegfs/cfDNA/protocol
 cd "$PROJECT_DIR"
 
 # Define directories for input and output files
