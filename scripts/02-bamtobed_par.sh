3a6ddc5942832cae3da29f176ace9475f181e38f
Darren Liu <64102138+dxl668@users.noreply.github.com>
2024-08-09 15:16:10 -0400

    Update 02-bamtobed_par.sh

--- scripts/02-bamtobed_par.sh
+++ scripts/02-bamtobed_par.sh
@@ -17,7 +17,7 @@
 # #SBATCH --mail-user=your.email@example.com
 
 # Define project directory
-PROJECT_DIR="/user/project"
+PROJECT_DIR=/user/project
 cd "$PROJECT_DIR"
 
 # Define directories for input and output files
