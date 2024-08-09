4957384b9f04bab3850acc29c3992a2337a8b072
Darren Liu <64102138+dxl668@users.noreply.github.com>
2024-08-09 15:39:03 -0400

    Update 10-frags_gc.sh

--- scripts/10-frags_gc.sh
+++ scripts/10-frags_gc.sh
@@ -30,8 +30,11 @@ mkdir -p "$outdir" "$plotdir" "$statdir" #Create directories if needed
 
 module load R/4.2.3
 
+# Define the path to the R script
+R_SCRIPT="${PROJECT_DIR}/scripts/10-frags_gc.R"
+
 # Run R script 
-Rscript 10-frags_gc.R \
+Rscript "$R_SCRIPT" \
 --motifdir $fragdir \
 --outdir $outdir \
 --plotdir $plotdir \
